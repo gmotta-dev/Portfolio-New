f253eb00579356884a3f1fdf44625d9342b2d99b
Gabriel Pacheco Motta <gpmotta21@gmail.com>
2024-06-09 12:20:05 -0300

    :zap: End Skills section

--- src/shared/components/Icons/index.tsx
+++ src/shared/components/Icons/index.tsx
@@ -1,2 +1,10 @@
 export { default as Link } from "./Link";
 export { default as Nextjs } from "./Nextjs";
+export { default as Typescript } from "./Typescript";
+export { default as Tailwind } from "./Tailwind";
+export { default as Figma } from "./Figma";
+export { default as Node } from "./Node";
+export { default as Sql } from "./Sql";
+export { default as Aws } from "./Aws";
+export { default as Git } from "./Git";
+
